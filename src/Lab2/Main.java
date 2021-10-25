5c82038fa54b577be961b6863498a1dc693cb38b
Kuba <pdrkuba@gmail.com>
2021-10-25 23:20:01 +0200

    jeden

---
+++ src/Lab2/Main.java
@@ -0,0 +1,17 @@
+package Lab2;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        String imie = args[0];
+        String pseudo = args[1];
+
+
+        System.out.println("Hello world! " + imie + " " + pseudo);
+
+        
+
+
+    }
+}
